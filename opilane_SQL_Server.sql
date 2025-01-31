7ec9df529e701e64c068fa1bad8f49f505e08323
OleksandraRyshniak <oleksandraryshniak@gmail.com>
2025-01-31 11:43:35 +0200

    Update opilane_SQL_Server.sql

--- opilane_SQL_Server.sql
+++ opilane_SQL_Server.sql
@@ -183,3 +183,119 @@ insert into oppimin
 
 select * from oppimine
 viga kood!
+
+
+CREATE DATABASE epoodRyshniak;
+use epoodRyshniak;
+
+CREATE TABLE Category (
+idCategory int Primary key identity(1,1),
+CategoryName varchar(25) UNIQUE
+);
+select * from Category;
+INSERT INTO Category (CategoryName)
+Values ('jook'), ('söök');
+
+-- TABELI struktuuri muutmine-->uue veergu lisamine
+Alter table Category ADD test int;
+
+-- TABELI struktuuri muutmine-->uue veergu kustamine
+Alter table Category DROP COLUMN test;
+
+create table Product (
+idProduct int Primary key identity(1,1), 
+ProductName varchar(25) UNIQUE, 
+idCategory int,
+Foreign key (idCategory) references Category(idCategory),
+Price decimal(2,1)
+)
+insert into Product(ProductName, idCategory, Price)
+VALUES
+('fanta',
+1,
+2.5),
+('sugar',
+2,
+0.7)
+select * from Product
+select * from Category
+
+create table Sale(
+idSale int primary key identity (1,1),
+idProduct int,
+Foreign key (idProduct) references Product(idProduct),
+idCustomer int,
+Count_ int,
+DateOfSale date);
+
+--kustutab kõik kirjed
+DELETE From Sale;
+
+Create table Customer(
+idCustomer int primary key identity (1,1),
+Name varchar(25),
+contact text)
+--tabeli muutmine --> FK lisamine
+Alter table Sale add foreign key (idCustomer) references Customer(idCustomer)
+--tabeli Customer täitmine
+-- tabeli Sale täitmine
+
+insert into Customer (Name, contact)
+values(
+'Nikita',
+'+37255905578'),
+('Oleksandra',
+'+37245780954'),
+('Mariia',
+'+37245678901'),
+('Stas',
+'+37245678901'),
+('Oleg',
+'+37267890123')
+select * from Customer
+delete from Customer
+insert into Sale (idProduct, idCustomer, Count_, DateofSale)
+values(
+1,
+1,
+12,
+'2025-01-30'),
+(3,
+2,
+2,
+'2025-01-30'),
+(4,
+3,
+1, 
+'2025-01-30'),
+(5,
+4,
+18,
+'2025-01-30'),
+(6,
+5,
+23,
+'2025-01-30')
+
+select * from Category
+select * from Product
+select * from Sale
+select * from Customer
+
+insert into Product(ProductName, idCategory, Price)
+VALUES 
+('mahl',
+1,
+2.2),
+('juust',
+2,
+1.5),
+('vesi',
+1,
+0.5),
+('fanta',
+1,
+2.5),
+('sugar',
+2,
+0.7)
