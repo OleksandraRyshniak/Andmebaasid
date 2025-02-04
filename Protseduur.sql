c84e2e3affa177ece642de82d1aee196174793ad
OleksandraRyshniak <oleksandraryshniak@gmail.com>
2025-02-04 13:37:48 +0200

    Create Protseduur.sql

---
+++ Protseduur.sql
@@ -0,0 +1,60 @@
+--SQl salvestatud protseduur - funktsioon, mis käivitab serveris mitu SQL tegevust järjest.
+Kasutame SQL Server
+
+Create database protseduurRyshniak;
+use protseduurRyshniak;
+Create table linn(
+linnId int primary key identity(1,1),
+linnNimi varchar(30),
+rahvaArv int);
+select * from linn;
+insert into linn(linnNimi, rahvaArv)
+Values('Tallinn', 60000), ('Tartu', 40000), ('Narva', 30000);
+
+--Protseduuri loomine
+--protseduur(fünksiion), mis lisab uus linn ja kohe näitab tabelis
+
+create procedure lisaLinn
+@lnimi varchar(30),
+@rArv int
+AS
+Begin
+insert into linn(linnNimi, rahvaArv)
+Values(@lnimi, @rArv);
+SELECT * FROM linn;
+end;
+
+--protseduuri kutse
+EXEC lisaLinn @lnimi='Pärnu', @rARv=1234
+--lihtsam
+EXEC lisaLinn 'Rakvere', 7895
+
+-- kirje kustutamine
+DELETE From linn Where linnID=5
+
+--protseduur, mis kustutab linn id järgi
+Create procedure kustutaLinn
+@deleteID int
+AS
+begin
+select * from linn;
+DELETE From linn Where linnID=@deleteID;
+select * from linn;
+end;
+
+--kutse
+EXEC kustutaLinn 4;
+--proceduri kustutamine
+DROP procedure kustutaLinn;
+
+--Protseduur, mis otsib linn esimese tähte järgi
+Create procedure linnaOtsing
+@taht char(1)
+AS
+begin
+SELECT * from linn
+Where linnNimi LIKE @taht + '%';
+--% - kõik teised tähed
+end;
+--kutse
+EXEC linnaOtsing T;
